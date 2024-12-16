5ff0c6669949ccc3ebe02c5677e6100343a622e1
ramxnnn <ramanpannu1003@gmail.com>
2024-12-16 17:18:58 -0500

    Updated the repo

--- lms/database/seeders/DatabaseSeeder.php
+++ lms/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Student::factory(100)->create();
         // User::factory(10)->create();
 
         User::factory()->create([
